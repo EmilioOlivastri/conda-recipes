61955abe6a4d5c326ddc6887d98eb5d966b01ba0
Emilio Olivastri <emilito.olivastri@gmail.com>
2026-04-24 14:52:39 +1000

    First (non-working) example on how to build a conda package with rattler/build

--- recipes/cyclonedds-py/build.sh
+++
@@ -1,2 +0,0 @@
-export CYCLONEDDS_HOME="$PREFIX/install"
-$PYTHON -m pip install .
--- recipes/unitree_sdk2/build.sh
+++
@@ -1,4 +0,0 @@
-set -exo pipefail
-
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR -DCMAKE_INSTALL_LIBDIR=lib
-ninja install
