7a5c2b57de04ad3573877ac83db8e5e82101420d
Emilio Olivastri <emilito.olivastri@gmail.com>
2026-04-27 11:15:49 +1000

    Working recipe of refractive colmap with CUDA for linux distributions

--- recipes/refractive_colmap/build.sh
+++ recipes/refractive_colmap/build.sh
@@ -3,8 +3,6 @@
 mkdir build
 cd build
 
-CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-
 if [[ ! -z "${cuda_compiler_version+x}" && "${cuda_compiler_version}" != "None" ]]
   then
     EXTRA_CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=all -DCUDA_ENABLED=ON"
@@ -12,20 +10,10 @@ if [[ ! -z "${cuda_compiler_version+x}" && "${cuda_compiler_version}" != "None" 
     EXTRA_CMAKE_ARGS="-DCUDA_ENABLED=OFF"
 fi
 
-if [[ "$target_platform" != "osx-64" ]]; then
-  EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DONNX_ENABLED=ON"
-else
-  EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DONNX_ENABLED=OFF"
-fi
-
 cmake ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
       -DBOOST_STATIC=OFF \
-      -DOPENGL_ENABLED=ON \
       -DBUILD_SHARED_LIBS=OFF \
-      -DFETCH_FAISS=OFF \
-      -DFETCH_POSELIB=OFF \
-      -DFETCH_ONNX=OFF \
       ${EXTRA_CMAKE_ARGS} \
 ..
 
